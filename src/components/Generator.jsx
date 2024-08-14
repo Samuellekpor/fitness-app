8a52a7d4aed8e7e5fac06dce3fb7b6f26e334764
Sammuel Kodzogan LEKPOR <samuellekpor@gmail.com>
2024-08-14 23:59:31 +0000

    Added function for workout generation

--- src/components/Generator.jsx
+++ src/components/Generator.jsx
@@ -18,11 +18,17 @@ function Header(props) {
   );
 }
 
-export default function Generator() {
+export default function Generator(props) {
   const [showModal, setShowModal] = useState(false);
-  const [poison, setPoison] = useState("individual");
-  const [muscles, setMuscles] = useState([]);
-  const [goal, setGoal] = useState("strength_power");
+  const {
+    poison,
+    setPoison,
+    muscles,
+    setMuscles,
+    goal,
+    setGoal,
+    updateWorkout,
+  } = props;
 
   function toggleModal() {
     setShowModal(!showModal);
@@ -65,8 +71,14 @@ export default function Generator() {
         {Object.keys(WORKOUTS).map((type, typeIndex) => {
           return (
             <button
-              onClick={() => {setPoison(type); setMuscles([]);}}
-              className={'bg-slate-950 border px-4 py-3 hover:border-blue-600 rounded-lg duration-200' + (type === poison ? ' border-blue-600' : 'border-blue-400')}
+              onClick={() => {
+                setPoison(type);
+                setMuscles([]);
+              }}
+              className={
+                "bg-slate-950 border px-4 py-3 hover:border-blue-600 rounded-lg duration-200" +
+                (type === poison ? " border-blue-600" : "border-blue-400")
+              }
               key={typeIndex}
             >
               <p className="capitalize">{type.replaceAll("_", " ")}</p>
@@ -85,17 +97,31 @@ export default function Generator() {
           onClick={toggleModal}
           className="relative flex px-4 py-3 items-center justify-center "
         >
-          <p className="capitalize">{muscles.length === 0 ? 'Select muscle groups' : muscles.join(" ")}</p>
+          <p className="capitalize">
+            {muscles.length === 0 ? "Select muscle groups" : muscles.join(" ")}
+          </p>
           <i className="fa-solid fa-caret-down absolute right-3 top-1/2 -translate-y-1/2"></i>
         </button>
         {showModal && (
           <div className="flex flex-col px-3 pb-3">
-            {(poison === "individual" ? WORKOUTS[poison] : Object.keys(WORKOUTS[poison])).map((muscleGroup, muscleGroupIndex) => {
-              return(
-                <button onClick={() => updateMuscles(muscleGroup)} key={muscleGroupIndex} className={"hover:text-blue-400 duration-200" + (muscles.includes(muscleGroup) ? ' text-blue-400' : '')}>
-                  <p className="uppercase">{muscleGroup.replaceAll("_", " ")}</p>
+            {(poison === "individual"
+              ? WORKOUTS[poison]
+              : Object.keys(WORKOUTS[poison])
+            ).map((muscleGroup, muscleGroupIndex) => {
+              return (
+                <button
+                  onClick={() => updateMuscles(muscleGroup)}
+                  key={muscleGroupIndex}
+                  className={
+                    "hover:text-blue-400 duration-200" +
+                    (muscles.includes(muscleGroup) ? " text-blue-400" : "")
+                  }
+                >
+                  <p className="uppercase">
+                    {muscleGroup.replaceAll("_", " ")}
+                  </p>
                 </button>
-              )
+              );
             })}
           </div>
         )}
@@ -106,11 +132,15 @@ export default function Generator() {
         title={"Become Juggernaut"}
         description={"Select your ultimate objective."}
       />
-      <div className="grid grid-cols-3 gap-4">
+      <div className="grid grid-cols-1 sm:grid-cols-3 gap-4">
         {Object.keys(SCHEMES).map((scheme, schemeIndex) => {
           return (
-            <button onClick={() => setGoal(scheme)}
-              className={"bg-slate-950 px-4 border hover:border-blue-600 py-3 rounded-lg duration-200" + (scheme === goal ? ' border-blue-600' : 'border-blue-400')}
+            <button
+              onClick={() => setGoal(scheme)}
+              className={
+                "bg-slate-950 px-4 border hover:border-blue-600 py-3 rounded-lg duration-200" +
+                (scheme === goal ? " border-blue-600" : "border-blue-400")
+              }
               key={schemeIndex}
             >
               <p className="capitalize">{scheme.replaceAll("_", " ")}</p>
@@ -118,7 +148,7 @@ export default function Generator() {
           );
         })}
       </div>
-      <Button text="Formulate" />
+      <Button func={updateWorkout} text="Formulate" />
     </SectionWrapper>
   );
 }
